3820b38a08f1febe988230d0a44364aa62e0dadd
Tainu <thainnosv@gmail.com>
2024-01-04 21:36:30 -0300

    create script

---
+++ script.js
@@ -0,0 +1,18 @@
+const btnGenerate = document.querySelector("#generate-pdf")
+
+btnGenerate.addEventListener("click", () => {
+
+    // Conteúdo do PDF
+    const content = document.querySelector("#content")
+    
+    // Configuração do arquivo final de PDF
+    const options = {
+        margin: [10, 10, 10, 10],
+        fillename: "nomeDoArquivo.pdf",
+        html2canvas: {sacle: 2},
+        jsPDF: {unit: "mm", format: "a4", orientation: "portrait"}
+    }
+
+    // Gerar e baixar o PdF
+    html2pdf().set(options).from(content).save()
+})
